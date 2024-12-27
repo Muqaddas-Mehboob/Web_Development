fb6bb3db4f4196a75c409ccdea5c34f6c18df712
Muqaddas-Mehboob <muqaddasmehboob7@gmail.com>
2024-12-27 02:56:29 -0800

    Update JS files

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
+// Printing syntax
 console.log("Hello");
+
+// Variables
+a = 10
+console.log(a);
+name1 = "Tony stark"
+console.log(name1);
+
+/*
+    Data Types (Primitive) [Note that charactrs are considered as string in js and there is no explicit representation 
+    of int and float and both will be considered as numbers]
+    -Numbers (Int, float)
+    -Boolean
+    -String (also Char)
+    -Undefined
+    -Null
+    -Bigint (Rarely used)
+    -Symbol (Rarely used)
+*/
+console.log(typeof a);
+console.log(typeof name1);
